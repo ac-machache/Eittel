608eb2a87c643a2a223b31803cb95e0aa795c4b0
machache <machache@eittel.com>
2025-10-28 12:40:31 +0100

    removed zep dep

--- src/eittel/tools/__init__.py
+++ src/eittel/tools/__init__.py
@@ -6,24 +6,8 @@ so LLMs do not need to pass identifiers.
 
 from __future__ import annotations
 
-from .zep_memory_tools import (
-    AddMemoriesTool,
-    add_memories,
-    SearchMemoriesTool,
-    search_memories,
-    GraphSearchTool,
-    search_graph,
-)
 from .context_agent_tool import ContextAgentTool
 
 __all__ = [
-    "AddMemoriesTool",
-    "add_memories",
-    "SearchMemoriesTool",
-    "search_memories",
-    "GraphSearchTool",
-    "search_graph",
     "ContextAgentTool",
 ]
-
-
